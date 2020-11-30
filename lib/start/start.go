1a360c4dcecec7ea25410644c48dc73aa5295311
Josh Ellis <josh@imjoshellis.com>
2020-11-30 15:47:24 -0600

    Update start function to automatically add numbers and prompt if not given args

--- lib/start/start.go
+++ lib/start/start.go
@@ -5,6 +5,9 @@ import (
 	"io"
 	"log"
 	"os"
+	"regexp"
+
+	"github.com/manifoldco/promptui"
 )
 
 func check(e error) {
@@ -15,19 +18,62 @@ func check(e error) {
 
 func main() {
 	var err error
+	var kata string
+
+	// Open solved katas folder to get names
+	f, err := os.Open("./solved")
+	check(err)
+	defer f.Close()
+	solved, err := f.Readdirnames(0)
+	check(err)
+
 	if len(os.Args) == 1 {
-		log.Fatal("You must include a kata name")
-		return
+		// If no args, prompt user with existing katas
+		names := filterNames(solved)
+		prompt := promptui.Select{
+			Label: "Select Existing Kata",
+			Items: names,
+		}
+		_, kata, err = prompt.Run()
+		check(err)
+	} else {
+		// otherwise, use the provided arg
+		kata = os.Args[1]
 	}
 
-	kata := os.Args[1]
+	// Count how many (if any) kata exist with this name
+	count := countExisting(solved, kata)
 
-	dir := fmt.Sprintf("katas/%v", kata) + "/"
+	// Set up the dir
+	dir := fmt.Sprintf("katas/%v%03d", kata, count) + "/"
 	err = os.Mkdir(dir, 0755)
 	check(err)
 
+	// Copy the files
 	copyMain(kata, dir)
 	copyTest(kata, dir)
+}
+
+func countExisting(solved []string, kata string) int {
+	count := 1
+	re := "^" + kata + "[0-9]+$"
+	for _, n := range solved {
+		if match, _ := regexp.Match(re, []byte(n)); match {
+			count++
+		}
+	}
+	return count
+}
+
+func filterNames(solved []string) []string {
+	names := []string{}
+	for _, name := range solved {
+		match, _ := regexp.Match("[a-zA-Z]+001", []byte(name))
+		if match {
+			names = append(names, name[:len(name)-3])
+		}
+	}
+	return names
 }
 
 func copyMain(k string, dir string) {
@@ -35,6 +81,7 @@ func copyMain(k string, dir string) {
 	main := fmt.Sprintf("%v.go", k)
 	new := dir + main
 	copyFile(old, new)
+	fmt.Printf("Created file: %s\n", new)
 }
 
 func copyTest(k string, dir string) {
@@ -42,6 +89,7 @@ func copyTest(k string, dir string) {
 	test := fmt.Sprintf("%v_test.go", k)
 	new := dir + test
 	copyFile(old, new)
+	fmt.Printf("Created file: %s\n", new)
 }
 
 func copyFile(old string, new string) {
